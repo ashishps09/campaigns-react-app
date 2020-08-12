047cd3360d3c69c1b15f6a9fbcfcc3a79cd1fea8
ashishps09 <68514138+ashishps09@users.noreply.github.com>
2020-08-12 20:54:20 +0530

    Updates

---
+++ precache-manifest.5ddb777b96b65aefd74c715f2cc2743b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816e2c0643cd630b97ab94f03948b014",
+    "url": "/campaigns-react-app/index.html"
+  },
+  {
+    "revision": "868d64f8cc35d4f60f49",
+    "url": "/campaigns-react-app/static/css/2.cedc9c11.chunk.css"
+  },
+  {
+    "revision": "cc14b4a231c433b4274d",
+    "url": "/campaigns-react-app/static/css/main.65ec61c7.chunk.css"
+  },
+  {
+    "revision": "868d64f8cc35d4f60f49",
+    "url": "/campaigns-react-app/static/js/2.6dc4e7dd.chunk.js"
+  },
+  {
+    "revision": "cc14b4a231c433b4274d",
+    "url": "/campaigns-react-app/static/js/main.0c5b6722.chunk.js"
+  },
+  {
+    "revision": "007cd9ce76c74c522efe",
+    "url": "/campaigns-react-app/static/js/runtime~main.fe6c0071.js"
+  },
+  {
+    "revision": "965fccbf2fa758c7e2e5fd0e96f016bb",
+    "url": "/campaigns-react-app/static/media/bluestacks_logo_cut.965fccbf.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/campaigns-react-app/static/media/garena.7173cfe7.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/campaigns-react-app/static/media/mancala_mix.977ecf7b.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/campaigns-react-app/static/media/mole_slayer.3ecfc5bd.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/campaigns-react-app/static/media/mortalcombat.90640a87.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/campaigns-react-app/static/media/needforspeed.c161ba4a.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/campaigns-react-app/static/media/pubg.e51f42c3.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/campaigns-react-app/static/media/super_jewels_quest.8a4d68db.png"
+  }
+]);
